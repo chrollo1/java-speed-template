c252e9f4fe22a14badcd46b1e5e0d8f9ca03d812
Steven Mathew <40929644+chrollo1@users.noreply.github.com>
2018-08-16 17:06:46 +0530

    comp-template

---
+++ comp-temp.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#define ll long long
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define maxn 15
+#define inf 0x3f3f3f3f
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) < (b) ? (b) : (a)
+#define lb lower_bound
+#define ub upper_bound
+#define vi vector<int>
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+
+using namespace std;
+
+inline void doit() {
+
+}
+
+int main() {
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+  
+  doit();
+  
+	return 0;
+}
+
+/*
+ * look for:
+ *  base/edge cases (n = 1, 0)
+ *	exact constraints
+ *	overflow (ll v int?)
+ *	array bounds
+ */
