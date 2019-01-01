11a731a130daf62a25d5c1938c8b4f2ca81143db
Steven Mathew <40929644+indra963@users.noreply.github.com>
2019-01-01 16:13:40 -0500

    Update comp-temp.cpp

--- comp-temp.cpp
+++ comp-temp.cpp
@@ -1,40 +1,32 @@
 #include <bits/stdc++.h>
 #include <stdio.h>
-#define ll long long
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+#define scan(x) do{while((x=getchar_unlocked())<'0'); for(x-='0'; '0'<=(_=getchar_unlocked()); x=(x<<3)+(x<<1)+_-'0');}while(0)
 char _;
-#define maxn 15
-#define inf 0x3f3f3f3f
+#define ll long long
+#define ull unsigned long long
+#define inf 1 << 30
 #define min(a, b) (a) < (b) ? (a) : (b)
 #define max(a, b) (a) < (b) ? (b) : (a)
-#define lb lower_bound
-#define ub upper_bound
 #define vi vector<int>
+#define vll vector<ll>
 #define pb push_back
 #define pii pair<int, int>
 #define mp make_pair
+#define f first
+#define s second
 #define mii map<int, int>
 #define umii unordered_map<int, int>
 
 using namespace std;
 
-inline void doit() {
-
-}
-
-int main() {
-	cin.tie(0);
+int main (int argc, char const *argv[]) {
+	#ifdef NOT_DMOJ
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif // NOT_DMOJ
 	cin.sync_with_stdio(0);
-  
-        doit();
-  
+	cin.tie(0);
+	cout.tie(0);
+    
 	return 0;
 }
-
-/*
- * look for:
- *  base/edge cases (n = 1, 0)
- *	exact constraints
- *	overflow (ll v int?)
- *	array bounds
- */
